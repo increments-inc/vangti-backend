eff048ef2f9772e5189f9c623661511b6dcc6ee8
faria-incrementsinc <saima.faria@incrementsinc.com>
2024-01-21 18:04:56 +0600

    refactor: analytics

--- web_socket/fcm.py
+++ web_socket/fcm.py
@@ -1,7 +1,9 @@
+import os
 import firebase_admin
-from firebase_admin import credentials, messaging
+from firebase_admin import credentials, messaging, auth
 from django.conf import settings
-
+# cred = credentials.Certificate(os.path.join(settings.BASE_DIR, "credentials.json"))
+# firebase_admin.initialize_app(cred)
 
 def send_push(title, msg, registration_token, data_object):
     message = messaging.MulticastMessage(
@@ -19,6 +21,21 @@ def send_push(title, msg, registration_token, data_object):
 
 
 def send_otp(title, msg, registration_token, data_object):
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=title,
+            body=msg,
+        ),
+        data=data_object,
+        tokens=registration_token
+    )
+    response = messaging.send_each_for_multicast(
+        message
+    )
+    print("successfully delivered", response)
+
+
+def send_phone(title, msg, registration_token, data_object):
     message = messaging.Message(
         notification=messaging.Notification(
             title=title,
