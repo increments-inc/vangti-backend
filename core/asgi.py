b057fe645e3373e1ee676915dd472d0dfa27c9e1
faria-incrementsinc <saima.faria@incrementsinc.com>
2024-03-25 10:26:56 +0600

    patch: asgi.py --> django.setup() added

--- core/asgi.py
+++ core/asgi.py
@@ -1,4 +1,6 @@
 import os
+
+import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -8,6 +10,7 @@ from web_socket.socket_middleware import QueryAuthMiddleware
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
 
 django_asgi_app = get_asgi_application()
 
