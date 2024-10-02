56098422691aec7042d1ce744b6ed2fabcd5ee1a
faria-incrementsinc <incincrements.com>
2024-10-02 14:49:29 +0600

    feat: log set in terminal

--- utils/log.py
+++ utils/log.py
@@ -6,6 +6,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     datefmt='%m/%d/%Y %H:%M:%S',
-    filename=f'{settings.LOG_DIR}/vangti.log',
+    # filename=f'{settings.LOG_DIR}/vangti.log',
 )
 
